d5007ef19fc52b5e5ff9a19a54a425b6da6d659d
Bkoura-eng <youssefsalem383@gmail.com>
2025-06-12 20:25:49 +0300

    test git

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// return json response
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Welcome to the API!']);
 });
