f27753afc3d1ecd6a793808cc1a52aa27fe421c6
Bkoura-eng <youssefsalem383@gmail.com>
2025-07-15 16:55:11 +0300

    blogs taks

--- app/Http/Resources/BlogResource.php
+++ app/Http/Resources/BlogResource.php
@@ -19,7 +19,8 @@ class BlogResource extends JsonResource
             // return is active as true or false
             'mark_as_hero' => $this->mark_as_hero ? true : false,
             'is_active' => $this->is_active ? true : false,
-            'created_by' => $this->author->email,
+            'created_by_email' => $this->author->email,
+            'created_by_name' => $this->author->name,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
